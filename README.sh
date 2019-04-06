5dc1da6a46e4b106a18d85b491201c3278fc0375
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2019-04-06 23:01:06 +0700

    Adding README.md

---
+++ README.sh
@@ -0,0 +1,14 @@
+
+# Twitter client
+
+This application doesn't include the consumer key and the consumer secret.
+The corresponding places in the source code where these values must be 
+are marked   with the _luwrain-twitter-consumer-key_ and _luwrain-twitter-consumer-secret_ strings.
+The substitution is performed during the compilation process  of our releases. 
+In the case you would like to create your own application on our code,
+you have to obtain new values from Twitter  using [this page](https://developer.twitter.com/apps).
+
+This repository is a part of the LUWRAIN project.
+LUWRAIN is a platform for the creation of apps for the blind and partially-sighted.
+Please visit [luwrain.org](http://luwrain.org/?lang=en) for further information.
+
